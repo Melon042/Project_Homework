4d36f2857979a663acf71e7996c24533bfafe24b
Dmitry <dmitry.babakov97@gmail.com>
2025-09-21 15:29:54 +0300

    REAMDE.md added

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,13 @@
-def filter_by_state(list_dict: list, state='EXECUTED') -> list:
-    """Функция принимает список словарей, фильтрует их по указанному значению ключа 'state' и возвращает отфильтрованный список."""
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+    """Принимает список словарей, фильтрует по указанному значению ключа 'state', возвращает отфильтрованный список."""
     list_filtered = []
     for item in list_dict:
-        if item.get('state') == state:
+        if item.get("state") == state:
             list_filtered.append(item)
     return list_filtered
 
 
 def sort_by_date(list_dict: list, reverse: bool = True) -> list:
-    """Функция принимает список словарей, сортирует их по дате и возвращает отсортированный список"""
-    list_sorted = sorted(list_dict, key=lambda x: x['date'], reverse=reverse)
+    """Принимает список словарей, сортирует их по дате и возвращает отсортированный список."""
+    list_sorted = sorted(list_dict, key=lambda x: x["date"], reverse=reverse)
     return list_sorted
