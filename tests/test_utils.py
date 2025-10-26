a034c6d73f89bde56323d1de110b94a0586a240e
Dmitry <dmitry.babakov97@gmail.com>
2025-10-26 09:10:29 +0300

    small docstring fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,6 +11,14 @@ def test_get_list_from_json_file_valid_json():
         assert result == [{"id": 1}, {"id": 2}]
 
 
+def test_get_list_from_json_file_invalid_json():
+    """Тест с не валидным JSON-списком."""
+    with patch("os.path.isfile", return_value=True), \
+         patch("builtins.open", mock_open(read_data="это не json")):
+        result = get_list_from_json_file("fake.json")
+        assert result == []
+
+
 def test_get_list_from_json_file_empty_file():
     """Тест с пустым файлом."""
     with patch("os.path.isfile", return_value=True), \
@@ -30,12 +38,5 @@ def test_get_list_from_json_file_not_a_list():
 def test_get_list_from_json_file_file_not_found():
     """Файл не существует."""
     with patch("os.path.isfile", return_value=False):
-        result = get_list_from_json_file("fake.json")
-        assert result == []
-
-
-def test_get_list_from_json_file_invalid_json():
-    with patch("os.path.isfile", return_value=True), \
-         patch("builtins.open", mock_open(read_data="это не json")):
         result = get_list_from_json_file("fake.json")
         assert result == []
