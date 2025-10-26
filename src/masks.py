9706052348af1f4888bf0876c4eefb778de6b7d6
Dmitry <dmitry.babakov97@gmail.com>
2025-10-26 18:56:01 +0300

    12.2 done

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,23 @@
+import logging
+
+logger = logging.getLogger('masks_logger')
+file_handler = logging.FileHandler('logs/masks.log', mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_num: str) -> str:
     """Маскирует номер карты."""
+    logger.info('Вызов функции "get_mask_card_number".')
+
     card_num = str(card_num)
 
     digits_only = "".join(char for char in card_num if char.isdigit())
 
     if len(digits_only) != 16:
+        logger.error('Ошибка ValueError, некорректная длина номера карты.')
         raise ValueError("Номер карты должен содержать 16 цифр")
 
     masked_card_num = f"{digits_only[:4]} {digits_only[4:6]}** **** {digits_only[12:]}"
@@ -16,17 +29,22 @@ def get_mask_card_number(card_num: str) -> str:
                 break
             prefix += char
         prefix = prefix.rstrip()
+        logger.info(f'Успешно замаскирован номер карты с префиксом: "{prefix}"')
         return f"{prefix} {masked_card_num}"
+    logger.info(f'Успешно замаскирован номер карты: "{masked_card_num}"')
     return masked_card_num
 
 
 def get_mask_account_number(account_num: str) -> str:
     """Маскирует номер счёта."""
+    logger.info('Вызов функции "get_mask_account_number".')
+
     account_num = str(account_num)
 
     digits_only = "".join(char for char in account_num if char.isdigit())
 
     if len(digits_only) != 20:
+        logger.error('Ошибка ValueError, некорректная длина номера счёта.')
         raise ValueError("Номер счёта должен содержать 20 цифр")
 
     masked_account_num = f"**{digits_only[-4:]}"
@@ -38,5 +56,7 @@ def get_mask_account_number(account_num: str) -> str:
                 break
             prefix += char
         prefix = prefix.rstrip()
+        logger.info(f'Успешно замаскирован номер счёта с префиксом: "{prefix}"')
         return f"{prefix} {masked_account_num}"
+    logger.info(f'Успешно замаскирован номер счёта: "{masked_account_num}"')
     return masked_account_num
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,36 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger('utils_logger')
+file_handler = logging.FileHandler('logs/utils.log', mode='w', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_list_from_json_file(path_to_json_file):
     """Принимает путь до JSON-файла и возвращает список.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
+    logger.info('Вызов функции "get_list_from_json_file".')
+
     try:
         if not os.path.isfile(path_to_json_file):
+            logger.warning('Файл не найден, возвращен пустой список.')
             return []
         with open(path_to_json_file, "r", encoding="utf-8") as file:
             data_json = file.read().strip()
         if not data_json:
+            logger.warning('Файл пуст, возвращен пустой список.')
             return []
         data = json.loads(data_json)
-        return data if isinstance(data, list) else []
-    except (json.JSONDecodeError, FileNotFoundError):
+        if isinstance(data, list):
+            logger.info('Файл успешно прочитан, возвращён список.')
+            return data
+        else:
+            logger.warning('Файл содержит не список, возвращён пустой список.')
+            return []
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        logger.error(f'Возникла ошибка: {e}, возвращен пустой список.')
         return []
