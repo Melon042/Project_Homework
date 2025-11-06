0a03b25fc5fa000d3fd2b44794089dc2b2b681d6
Dmitry <dmitry.babakov97@gmail.com>
2025-11-06 07:16:52 +0300

    funcs added: process_bank_search, count_categories. (tasks 1 and 2)

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,7 @@
+import re
+from collections import Counter
+
+
 def filter_by_state(bank_operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Принимает список словарей, фильтрует по указанному значению ключа "state", возвращает отфильтрованный список."""
     filtered_bank_operations = []
@@ -13,3 +17,35 @@ def sort_by_date(bank_operations: list[dict], reverse: bool = True) -> list[dict
         bank_operations, key=lambda bank_operation: bank_operation["date"], reverse=reverse
     )
     return sorted_bank_operations
+
+
+def process_bank_search(data:list[dict], search:str)->list[dict]:
+    """Принимает список словарей с данными о банковских операциях и строку поиска,
+    возвращает список словарей, у которых в описании есть данная строка"""
+    result = []
+
+    for operation in data:
+        if re.search(search, operation['description']):
+            result.append(operation)
+    return result
+
+
+def count_categories(data:list[dict], categories_list:list) -> dict:
+    """Принимает список словарей с данными о банковских операциях и список категорий операций,
+    возвращает словарь, в котором ключи — это названия категорий, а значения — это количество
+    операций в каждой категории"""
+    categories = []
+
+    for operation in data:
+        for category in categories_list:
+            if category == operation['description']:
+                categories.append(category)
+                break
+
+    counted_categories = Counter(categories)
+
+    for category in categories_list:
+        if category not in counted_categories.keys():
+            counted_categories[category] = 0
+
+    return dict(counted_categories)
